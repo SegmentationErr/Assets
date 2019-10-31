3e084ee74616da285b85d706e52833be1a9f61c0
SegmentationErr <36937907+SegmentationErr@users.noreply.github.com>
2019-10-31 13:25:06 +1100

    add minimap

--- Scripts/PickupTrigger.cs
+++ Scripts/PickupTrigger.cs
@@ -12,18 +12,32 @@ public class PickupTrigger : MonoBehaviour
         {
             gameObject.GetComponent<WeaponControl>().enabled = false;
         }
+        // gameObject.tag = "Enemy";
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        
+        // Debug.Log("1"+gameObject.tag);
+        // Debug.Log("2"+gameObject.tag == "Potion");
+        // Debug.Log("3"+gameObject.CompareTag("Potion"));
+        if (gameObject.CompareTag("Potion")) {
+            Debug.Log("oooooo");
+        } 
+
         if (other.gameObject.tag == "Player")
         {
-            this.transform.SetParent(hand.transform);
-            this.gameObject.SetActive(false);
-            transform.localPosition = new Vector3(0.03399992f, -0.068f,0);
-            gameObject.GetComponent<WeaponControl>().enabled = true;
+            Debug.Log(gameObject.tag);
+
+            if (gameObject.tag == "Potion") {
+                Debug.Log("oooooo");
+            }
+            else {
+                this.transform.SetParent(hand.transform);
+                this.gameObject.SetActive(false);
+                transform.localPosition = new Vector3(0.03399992f, -0.068f,0);
+                gameObject.GetComponent<WeaponControl>().enabled = true;
+            }
         }
-        
-        
     }
 }
