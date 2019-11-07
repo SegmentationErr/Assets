2d9d3c6ae34483a5a847afa0b46108cb3bbccf5b
Li Ding <z5138091@unsw.edu.au>
2019-11-07 19:36:14 +1100

    main room

---
+++ Scripts/OpenShop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenShop : MonoBehaviour
+{
+    private GameObject shop;
+    private GameObject UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI = GameObject.Find("UI");
+        shop = UI.transform.Find("Shop UI").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            shop.SetActive(true);
+        }
+
+    }
+}
---
+++ Scripts/QuitShop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitShop : MonoBehaviour
+{
+    private GameObject shop;
+    private GameObject UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI = GameObject.Find("UI");
+        shop = UI.transform.Find("Shop UI").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            shop.SetActive(false);
+        }
+
+    }
+}
