93837e15ec3c22dab7af7f1b94e5c3ecf9eecd94
SegmentationErr <36937907+SegmentationErr@users.noreply.github.com>
2019-11-14 04:31:34 +1100

    fix room bugs and weapon can change direction

--- Scripts/WeaponControl.cs
+++ Scripts/WeaponControl.cs
@@ -67,6 +67,13 @@ public class WeaponControl : MonoBehaviour
             Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
             float rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+            // print(rotz);
+            if (((rotz < -90f || rotz > 90f) && transform.localScale.y > 0) ||
+                (rotz >= -90f && rotz <= 90f && transform.localScale.y < 0)) {
+                transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+            }
+
             transform.rotation = Quaternion.Euler(0f, 0f, rotz);
             if ((ws.energy.CurrentVal - energyConsume) >= 0)
             {
