f07a82f80d70005ed33ea21da02ddd72ffb7fe19
ARnnnooo51 <fanchenming51@gmail.com>
2019-11-23 17:48:12 +1100

    save game

---
+++ Scripts/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class PlayerData
+{
+    // Start is called before the first frame update
+    public int level;
+    public int coin;
+
+    public PlayerData(PlayerControl  player){
+    	coin = player.getCoin();
+        level = PlayerPrefs.GetInt("LevelReached");
+    	
+    }
+}
---
+++ Scripts/SaveSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+public static class SaveSystem
+{
+	public static void SavePlayer(PlayerControl player){
+		BinaryFormatter formatter = new BinaryFormatter();
+		string path = Application.persistentDataPath + "/player.fun";
+		FileStream stream = new FileStream(Path,FileMode.Create);
+
+		PlayerDate data = new PlayerDate(player);
+
+		formatter.Serialize(stream, data);
+		stream.Close();
+	}
+
+	public static void 
+}
